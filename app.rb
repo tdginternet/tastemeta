bb55261867a9b5952b345cb073a53fff6ee85dc9
Jonas Porto <jporto.contato@hotmail.com>
2017-07-11 14:19:05 -0300

    detecting wether is browser or bot

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-require 'sinatra'
+require 'browser'
 require 'json'
 require 'nokogiri'
 require 'open-uri'
+require 'sinatra'
 
 require_relative 'config/redis'
 
@@ -24,4 +25,19 @@ post '/' do
   metas.map do |meta|
   	Hash[meta.attributes["property"].value, meta.attributes["content"].value]
   end.to_json
+end
+
+get '/:shortened' do
+  if browser.bot?
+    erb :page
+  else
+    content_type :json
+    params.to_json
+  end
+end
+
+private
+
+def browser
+  @_browser ||= Browser.new(request.user_agent)
 end
