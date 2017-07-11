da1fe95d0fc40f2f2dc37a745af7135ed7fe4342
Jonas Porto <jporto.contato@hotmail.com>
2017-07-11 15:59:21 -0300

    create a new form to edit meta tags

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
-require 'browser'
+require 'sinatra'
+require 'sinatra/cross_origin'
 require 'json'
 require 'nokogiri'
 require 'open-uri'
-require 'sinatra'
-require 'sinatra/cross_origin'
+require "digest/md5"
+require 'browser'
 
 require_relative 'config/redis'
 
@@ -11,11 +12,6 @@ register Sinatra::CrossOrigin
 
 configure do
   enable :cross_origin
-end
-
-before do
-  puts '[Params]'
-  p params
 end
 
 get '/' do
@@ -27,20 +23,43 @@ post '/' do
   page  = Nokogiri::HTML(open(url))
   metas = page.css("meta[property^=og]")
 
-  content_type :json
-
-  metas.map do |meta|
+  metas = metas.map do |meta|
   	Hash[meta.attributes["property"].value, meta.attributes["content"].value]
   end.to_json
+
+  shortened_id = $redis.incr("urls._id").to_s(36)
+  $redis.hset("tastemeta:#{shortened_id}", url, metas)
+
+  redirect "/#{shortened_id}?inspect"
 end
 
-get '/:shortened' do
+get '/:shortened_id' do
+  shortened_id = params['shortened_id']
+  results = $redis.hgetall("tastemeta:#{shortened_id}")
+
+  @metas = JSON.parse(results.values.first)
+  @origin_url = results.keys.first
+
   if browser.bot?
     erb :page
+  elsif params.key?('inspect')
+    erb :inspect
   else
-    content_type :json
-    params.to_json
+    redirect @origin_url
   end
+end
+
+post '/:shortened_id' do
+  shortened_id = params['shortened_id']
+  metas        = params['metas']
+  url          = params['origin_url']
+
+  # convert to array of hashes
+  metas = metas.map{|meta| Hash[*meta]}.to_json
+
+  $redis.hset("tastemeta:#{shortened_id}", url, metas)
+
+  redirect "/#{shortened_id}?inspect"
 end
 
 private
